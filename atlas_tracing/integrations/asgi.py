9173a639ceb70faee2af03eb6195ca14d4d0d46d
Guilherme Scaranse <guiscaranse@gmail.com>
2020-12-18 13:07:08 -0300

    Properly add status code to parent span

--- atlas_tracing/integrations/asgi.py
+++ atlas_tracing/integrations/asgi.py
@@ -1,7 +1,10 @@
 from fnmatch import fnmatch
+from functools import wraps
 from typing import List
 
-from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
+from opentelemetry import context, propagators, trace
+from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware, set_status_code, \
+    collect_request_attributes, carrier_getter
 from opentelemetry.instrumentation.fastapi.version import __version__  # noqa
 
 
@@ -17,7 +20,50 @@ class ASGIMiddleware(OpenTelemetryMiddleware):
         if scope["type"] not in ("http", "websocket"):
             return await self.app(scope, receive, send)
         if await self.is_filtered(scope):
-            response = await self.app(scope, receive, send)
-        else:
-            response = await super().__call__(scope, receive, send)
-        return response
+            return await self.app(scope, receive, send)
+
+        token = context.attach(propagators.extract(carrier_getter, scope))
+        span_name, additional_attributes = self.span_details_callback(scope)
+
+        try:
+            with self.tracer.start_as_current_span(
+                    span_name + " asgi", kind=trace.SpanKind.SERVER,
+            ) as span:
+                if span.is_recording():
+                    attributes = collect_request_attributes(scope)
+                    attributes.update(additional_attributes)
+                    for key, value in attributes.items():
+                        span.set_attribute(key, value)
+
+                @wraps(receive)
+                async def wrapped_receive():
+                    with self.tracer.start_as_current_span(
+                            span_name + " asgi." + scope["type"] + ".receive"
+                    ) as receive_span:
+                        message = await receive()
+                        if receive_span.is_recording():
+                            if message["type"] == "websocket.receive":
+                                set_status_code(receive_span, 200)
+                            receive_span.set_attribute("type", message["type"])
+                    return message
+
+                @wraps(send)
+                async def wrapped_send(message):
+                    with self.tracer.start_as_current_span(
+                            span_name + " asgi." + scope["type"] + ".send"
+                    ) as send_span:
+                        if send_span.is_recording():
+                            if message["type"] == "http.response.start":
+                                status_code = message["status"]
+                                set_status_code(send_span, status_code)
+                                set_status_code(span, status_code)
+
+                        elif message["type"] == "websocket.send":
+                            set_status_code(send_span, 200)
+                            set_status_code(span, 200)
+                        send_span.set_attribute("type", message["type"])
+                        await send(message)
+
+                await self.app(scope, wrapped_receive, wrapped_send)
+        finally:
+            context.detach(token)
